8304377957364ad0fff69f7b07c339e2bbdd89e1
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-08-05 18:38:24 +0200

    feat(core): Add Hvtdp theme

---
+++ composeApp/src/commonMain/kotlin/com.sjaindl/s11/theme/Dimensions.kt
@@ -0,0 +1,18 @@
+package com.sjaindl.s11.theme
+
+import androidx.compose.ui.unit.dp
+
+class Dimensions {
+   val spacing = Spacing()
+
+   class Spacing() {
+      val none = 0.dp
+      val xxs = 4.dp
+      val xs = 8.dp
+      val s = 12.dp
+      val md = 16.dp
+      val lg = 24.dp
+      val xl = 32.dp
+      val xxl = 48.dp
+   }
+}
