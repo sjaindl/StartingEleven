65d1241389f63f72b907aae1880d78f3d2d3c799
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-09-02 22:33:30 +0200

    fix(core): Make FallbackImage zoomable & translatable

--- core/src/commonMain/kotlin/com/sjaindl/s11/core/baseui/FallbackImage.kt
+++ core/src/commonMain/kotlin/com/sjaindl/s11/core/baseui/FallbackImage.kt
@@ -2,18 +2,37 @@ package com.sjaindl.s11.core.baseui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import coil3.compose.LocalPlatformContext
 import coil3.compose.SubcomposeAsyncImage
 import coil3.request.CachePolicy
 import coil3.request.ImageRequest
+import com.sjaindl.s11.core.theme.spacing
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
@@ -28,24 +47,128 @@ fun FallbackImage(
     cacheEnabled: Boolean = true,
     additionalCacheKey: String? = null,
 ) {
-    Column {
+    var isFullscreen by remember {
+        mutableStateOf(value = false)
+    }
+
+    var scale by remember {
+        mutableStateOf(value = 1f)
+    }
+
+    var offsetX by remember {
+        mutableStateOf(value = 0f)
+    }
+
+    var offsetY by remember {
+        mutableStateOf(value = 0f)
+    }
+
+    if (isFullscreen) {
+        Dialog(
+            onDismissRequest = {
+                isFullscreen = false
+            },
+            properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true,
+                usePlatformDefaultWidth = false,
+            )
+        ) {
+            Box(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(vertical = spacing.xxl),
+                contentAlignment = Alignment.TopEnd,
+            ) {
+                val imageModifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        translationX = offsetX,
+                        translationY = offsetY,
+                        transformOrigin = TransformOrigin(pivotFractionX = 0.5f, pivotFractionY = 0.5f),
+                    )
+                    .pointerInput(Unit) {
+                        detectTransformGestures { centroid, pan, zoom, rotation ->
+                            scale = (scale * zoom).coerceAtLeast(minimumValue = 1f) // Limit zoom to 1x minimum
+                            offsetX += pan.x * scale
+                            offsetY += pan.y * scale
+                        }
+                    }
+
+                if (!photoRefDownloadUrl.isNullOrEmpty()) {
+                    AsyncImage(
+                        url = photoRefDownloadUrl,
+                        cacheEnabled = cacheEnabled,
+                        additionalCacheKey = additionalCacheKey,
+                        modifier = imageModifier,
+                        contentScale = ContentScale.Fit,
+                    )
+                } else if (!photoUrl.isNullOrEmpty()) {
+                    AsyncImage(
+                        url = photoUrl,
+                        cacheEnabled = cacheEnabled,
+                        additionalCacheKey = additionalCacheKey,
+                        modifier = imageModifier,
+                        contentScale = ContentScale.Fit,
+                    )
+                }
+
+                IconButton(
+                    onClick = {
+                        isFullscreen = false
+                    },
+                    content = {
+                        Icon(
+                            imageVector = Icons.Rounded.Close,
+                            contentDescription = null,
+                        )
+                    }
+                )
+
+            }
+        }
+    } else {
         if (!photoRefDownloadUrl.isNullOrEmpty()) {
-            AsyncImage(url = photoRefDownloadUrl, cacheEnabled = cacheEnabled, additionalCacheKey = additionalCacheKey)
+            AsyncImage(
+                url = photoRefDownloadUrl,
+                cacheEnabled = cacheEnabled,
+                additionalCacheKey = additionalCacheKey,
+                modifier = Modifier
+                    .clickable {
+                        isFullscreen = true
+                    },
+            )
         } else if (!photoUrl.isNullOrEmpty()) {
-            AsyncImage(url = photoUrl, cacheEnabled = cacheEnabled, additionalCacheKey = additionalCacheKey)
+            AsyncImage(
+                url = photoUrl,
+                cacheEnabled = cacheEnabled,
+                additionalCacheKey = additionalCacheKey,
+                modifier = Modifier
+                    .clickable {
+                        isFullscreen = true
+                    },
+            )
         } else {
             Image(
                 painter = painterResource(fallback),
                 contentDescription = null,
                 modifier = Modifier
-                    .size(40.dp),
+                    .size(size = 40.dp),
             )
         }
     }
 }
 
 @Composable
-private fun AsyncImage(url: String, cacheEnabled: Boolean, additionalCacheKey: String?) {
+private fun AsyncImage(
+    url: String,
+    cacheEnabled: Boolean,
+    additionalCacheKey: String?,
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Crop,
+) {
     val context = LocalPlatformContext.current
 
     val cachePolicy = if (cacheEnabled) {
@@ -66,7 +189,7 @@ private fun AsyncImage(url: String, cacheEnabled: Boolean, additionalCacheKey: S
         .build()
 
     SubcomposeAsyncImage(
-        modifier = Modifier
+        modifier = modifier
             .size(size = 120.dp),
         model = imageRequest,
         loading = {
@@ -77,7 +200,7 @@ private fun AsyncImage(url: String, cacheEnabled: Boolean, additionalCacheKey: S
                     .padding(all = 16.dp),
             )
         },
-        contentScale = ContentScale.Crop,
+        contentScale = contentScale,
         contentDescription = null,
     )
 }
