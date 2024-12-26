1ff018b7f281fc2371669db429d11f24c7b81630
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-12-26 20:27:07 +0100

    feat(chore): Adapt Android release version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -203,8 +203,8 @@ android {
 
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         val facebookClientToken = gradleLocalProperties(rootDir, providers).getProperty("facebookClientToken")
         manifestPlaceholders["facebookClientToken"] = facebookClientToken
