4ca17e0ea5a148b9cb215f7db44c2f6f7ad24cfc
Stefan Jaindl <jaindl.stefan@gmail.com>
2025-12-22 16:03:02 +0100

    feat(AI): Improve chat screen UI

--- ai/src/commonMain/kotlin/com/sjaindl/s11/ai/ui/ChatScreen.kt
+++ ai/src/commonMain/kotlin/com/sjaindl/s11/ai/ui/ChatScreen.kt
@@ -1,15 +1,26 @@
 package com.sjaindl.s11.ai.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedAssistChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sjaindl.s11.ai.data.remote.model.SourceDocument
@@ -27,10 +39,13 @@ import com.sjaindl.s11.ai.data.remote.model.Tool
 import com.sjaindl.s11.ai.ui.components.SourceDocumentsDialog
 import com.sjaindl.s11.ai.ui.components.UsedToolsDialog
 import com.sjaindl.s11.core.baseui.ErrorScreen
-import com.sjaindl.s11.core.baseui.LoadingScreen
 import com.sjaindl.s11.core.theme.HvtdpTheme
 import com.sjaindl.s11.core.theme.spacing
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import startingeleven.ai.generated.resources.Res
+import startingeleven.ai.generated.resources.chat_user
+import startingeleven.ai.generated.resources.chatbot
 
 @Composable
 fun ChatScreen() {
@@ -66,33 +81,45 @@ fun ChatScreen() {
                 .weight(1f),
         ) {
             items(uiState.messages) { message ->
-                Text(
-                    text = "${if (message.isFromUser) "You" else "AI"}: ${message.text}",
+                Card(
                     modifier = Modifier
-                        .padding(8.dp),
-                )
-
-                Row(horizontalArrangement = Arrangement.spacedBy(spacing.s)) {
-                    message.usedTools?.let { tools ->
-                        ElevatedAssistChip(
-                            onClick = {
-                                showToolsDialogFor = tools
-                            },
-                            label = {
-                                Text("Used Tools (${tools.size})")
-                            }
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Image(
+                            painter = painterResource(if (message.isFromUser) Res.drawable.chat_user else Res.drawable.chatbot),
+                            contentDescription = if (message.isFromUser) "User" else "AI",
+                            modifier = Modifier.size(40.dp).clip(CircleShape)
                         )
-                    }
 
-                    message.sourceDocuments?.let { docs ->
-                        ElevatedAssistChip(
-                            onClick = {
-                                showSourceDocsDialogFor = docs
-                            },
-                            label = {
-                                Text("Source Documents (${docs.size})")
+                        Spacer(modifier = Modifier.size(8.dp))
+
+                        Column {
+                            Text(text = message.text)
+
+                            Row(horizontalArrangement = Arrangement.spacedBy(spacing.s)) {
+                                message.usedTools?.let { tools ->
+                                    ElevatedAssistChip(
+                                        onClick = { showToolsDialogFor = tools },
+                                        label = { Text("Used Tools (${tools.size})") },
+                                        colors = AssistChipDefaults.elevatedAssistChipColors(containerColor = MaterialTheme.colorScheme.secondaryContainer)
+                                    )
+                                }
+
+                                message.sourceDocuments?.let { docs ->
+                                    ElevatedAssistChip(
+                                        onClick = { showSourceDocsDialogFor = docs },
+                                        label = { Text("Source Documents (${docs.size})") },
+                                        colors = AssistChipDefaults.elevatedAssistChipColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                                    )
+                                }
                             }
-                        )
+                        }
                     }
                 }
             }
@@ -117,14 +144,14 @@ fun ChatScreen() {
         }
 
         if (uiState.isLoading) {
-            LoadingScreen()
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
         }
 
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(spacing.s),
+            horizontalArrangement = Arrangement.spacedBy(spacing.md),
             verticalAlignment = Alignment.CenterVertically,
         ) {
             TextField(
@@ -143,7 +170,7 @@ fun ChatScreen() {
                 },
                 enabled = prompt.isNotBlank() && !uiState.isLoading
             ) {
-                Text(text = "Send")
+                Icon(imageVector = Icons.AutoMirrored.Filled.Send, contentDescription = null)
             }
         }
     }
